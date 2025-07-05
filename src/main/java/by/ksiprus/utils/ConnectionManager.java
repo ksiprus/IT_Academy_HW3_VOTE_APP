5ca208e7d21ae7189fd5c768844e577fdfa9e567
Uladzimir Shunevich <ksiprus@mail.ru>
2025-07-05 14:52:04 +0300

    изменён класс ConnectionManager

--- src/main/java/by/ksiprus/utils/ConnectionManager.java
+++ src/main/java/by/ksiprus/utils/ConnectionManager.java
@@ -1,37 +1,33 @@
 package by.ksiprus.utils;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public final class ConnectionManager {
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "1234";
-    private static final String URL = "jdbc:postgresql://localhost:5433/test";
+    private static final ComboPooledDataSource dataSource;
 
     static {
-        loadDriver();
-    }
-
-    private static void loadDriver() {
         try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            dataSource = new ComboPooledDataSource();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка инициализации пула  соединений!!!", e);
         }
     }
 
     private ConnectionManager() {
     }
-  
 
-    public static Connection openConnection() {
-        try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public static Connection openConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 
+    public static void closePool() {
+        if (dataSource == null) {
+            dataSource.close();
+        }
+    }
 }
 
